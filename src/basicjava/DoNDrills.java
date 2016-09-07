99464bc7eccc58cd61ac78918a593fa1cfd307ea
andrewmizener <almALM123>
2016-09-07 11:51:19 -0500

    Code

---
+++ src/basicjava/DoNDrills.java
@@ -0,0 +1,18 @@
+package basicjava;
+
+/**
+ * Created by andrewmizener on 9/7/16.
+ */
+public class DoNDrills {
+    public static void main(String[] args){
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
+        for (int a = 0; a < 11; a++){
+            System.out.println(a);
+        }
+        for (int b = 10; b >= 0; b--){
+            System.out.println(b);
+        }
+    }
+}
